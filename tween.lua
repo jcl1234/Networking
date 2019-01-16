a354e91abc1b50fb11d3cfb1b6f03aee57a3938f
jcl1234 <jacoblowery1@gmail.com>
2019-01-15 19:23:54 -0600

    optimized tweening

--- tween.lua
+++ tween.lua
@@ -1,14 +1,16 @@
 local tween = {}
 tween.tweens = {}
 
---Copy table
-local function copy(t)
-	if type(t) ~= "table" or t == {} then return t end
-	local new = {}
-	for k,v in pairs(t) do
-		new[k] = copy(v)
+local function getDifs(startT, endT)
+	local difs = {}
+	for k,v in pairs(startT) do
+		if type(v) == "table" then
+			difs[k] = getDifs(v, endT[k])
+		elseif type(v) == "number" then
+			difs[k] = endT[k] - v
+		end
 	end
-	return new
+	return difs
 end
 
 function tween.new(time, startT, endT)
@@ -20,6 +22,15 @@ function tween.new(time, startT, endT)
 	t.origVals = copy(t.vals)
 	t.endVals = endT
 
+	--Copy original values
+	for k,v in pairs(t.vals) do
+		t.origVals[k] = v
+	end
+
+	t.difs = getDifs(t.vals, t.endVals)
+
+
+
 	table.insert(tween.tweens, t)
 	return t
 end
@@ -28,12 +39,14 @@ function tween.update(dt)
 	for k, tw in pairs(tween.tweens) do
 		tw.timeLeft = tw.timeLeft - dt
 		for k, v in pairs(tw.vals) do
-			if tw.timeLeft <= 0 then
-				tw.vals[k] = tw.endVals[k]
-			else
-				local dif = tw.endVals[k] - tw.origVals[k]
-				local newVal = tw.vals[k] + (dif/tw.time)*dt
-				tw.vals[k] = newVal
+			if type(v) == "number" then
+				if tw.timeLeft <= 0 then
+					tw.vals[k] = tw.endVals[k]
+				else
+					local dif = tw.difs[k]
+					local newVal = tw.vals[k] + (dif/tw.time)*dt
+					tw.vals[k] = newVal
+				end
 			end
 		end
 		if tw.timeLeft <= 0 then
